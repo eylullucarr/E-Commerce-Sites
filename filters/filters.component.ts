714892f01e8dbd5713dc030a685764f81bb59dc4
Eylül Uçar <97471640+eylullucarr@users.noreply.github.com>
2023-08-02 16:40:54 +0300

    hotfix

---
+++ filters/filters.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filters',
+  templateUrl: './filters.component.html',
+  styleUrls: ['./filters.component.css'],
+  standalone:true,
+  imports: [ CommonModule ]
+})
+export class FiltersComponent {
+
+}
